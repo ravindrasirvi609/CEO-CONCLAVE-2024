c757a7f79461d1cff8cd5f23186267372e64c9ce
Ravindra <sirviravindra609@gmail.com>
2024-10-19 15:58:04 +0530

    refactor: Update registration options in RegistrationSection component

--- src/app/components/RegistrationSection.tsx
+++ src/app/components/RegistrationSection.tsx
@@ -307,9 +307,9 @@ const RegistrationSection: React.FC = () => {
                 className="block w-full bg-white border-2 border-[#E0A75E] text-[#6c0707] py-3 px-4 rounded-md leading-tight focus:outline-none focus:border-[#6c0707]"
               >
                 <option value="student">Student (1770 ₹)</option>
-                <option value="teacher">Academia (3000 ₹)</option>
+                <option value="teacher">Academia (2500 ₹)</option>
                 <option value="professional">
-                  Industry Professional (5000 ₹)
+                  Industry Professional (3500 ₹)
                 </option>
                 <option value="opfmembers">OPF Members (1500 ₹)</option>
               </select>
